808b010f183c26d0dcf958f0454d233acb92799b
tory4312 <86555871+tory4312@users.noreply.github.com>
2023-07-16 16:42:33 +0200

    progress

--- main.cpp
+++ main.cpp
@@ -10,39 +10,47 @@
 #include <vector>
 #include <algorithm>
 #include <sys/poll.h>
+#include <sstream>
 
-struct ServerConfig {
+struct ServerConfig
+{
     std::string host;
     int port;
 };
 
-std::map<std::string, ServerConfig> parseConfiguration(const std::string& configFile) {
+std::map<std::string, ServerConfig> parseConfiguration(const std::string &configFile)
+{
     std::map<std::string, ServerConfig> configMap;
 
     std::ifstream file(configFile);
-    if (!file) {
+    if (!file)
+    {
         std::cerr << "Failed to open configuration file: " << configFile << std::endl;
         return configMap;
     }
 
     std::string line;
     std::string currentServer;
-    while (std::getline(file, line)) {
+    while (std::getline(file, line))
+    {
         if (line.empty() || line[0] == '#')
             continue;
 
-        if (line.substr(0, 7) == "server ") {
+        if (line.substr(0, 7) == "server ")
+        {
             currentServer = line.substr(7);
             configMap[currentServer] = ServerConfig();
         }
-        else if (line.find("host") != std::string::npos) {
+        else if (line.find("host") != std::string::npos)
+        {
             size_t index = line.find("host") + 5;
             configMap[currentServer].host = line.substr(index);
             configMap[currentServer].host.erase(0, configMap[currentServer].host.find_first_not_of(" \t"));
             configMap[currentServer].host.erase(configMap[currentServer].host.find_last_not_of(" \r\n\t") + 1);
             std::cout << "host: " << configMap[currentServer].host << std::endl;
         }
-        else if (line.find("port") != std::string::npos) {
+        else if (line.find("port") != std::string::npos)
+        {
             size_t index = line.find("port") + 5;
             configMap[currentServer].port = std::stoi(line.substr(index));
             std::cout << "port: " << configMap[currentServer].port << std::endl;
@@ -53,9 +61,26 @@ std::map<std::string, ServerConfig> parseConfiguration(const std::string& config
     return configMap;
 }
 
-int main(int argc, char** argv) {
+void handleGetRequest(int clientSocket, const std::string &url)
+{
+    std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+    response += "Hello from the server! You requested: " + url;
+    send(clientSocket, response.c_str(), response.length(), 0);
+}
+
+void handlePostRequest(int clientSocket, const std::string &url, const std::string &payload)
+{
+    std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+    response += "Hello from the server! You sent a POST request to: " + url + "\n";
+    response += "Payload: " + payload;
+    send(clientSocket, response.c_str(), response.length(), 0);
+}
+
+int main(int argc, char **argv)
+{
     std::string configFile = "default.conf";
-    if (argc > 1) {
+    if (argc > 1)
+    {
         configFile = argv[1];
     }
 
@@ -65,14 +90,16 @@ int main(int argc, char** argv) {
     std::vector<pollfd> pollfds;
 
     std::map<std::string, ServerConfig>::iterator it;
-    int listenSocket; // Declare listenSocket outside the loop
-    for (it = configMap.begin(); it != configMap.end(); ++it) {
-        const std::string& serverName = it->first;
-        const ServerConfig& serverConfig = it->second;
+    int listenSocket = 0;
+    for (it = configMap.begin(); it != configMap.end(); ++it)
+    {
+        const std::string &serverName = it->first;
+        const ServerConfig &serverConfig = it->second;
         std::cout << "Starting server: " << serverName << " on " << serverConfig.host << ":" << serverConfig.port << std::endl;
 
         listenSocket = socket(AF_INET, SOCK_STREAM, 0);
-        if (listenSocket < 0) {
+        if (listenSocket < 0)
+        {
             std::cerr << "Failed to create a socket." << std::endl;
             exit(EXIT_FAILURE);
         }
@@ -83,13 +110,15 @@ int main(int argc, char** argv) {
         serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
         serverAddress.sin_port = htons(serverConfig.port);
 
-        if (bind(listenSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+        {
             std::cerr << "Failed to bind the socket to the address." << std::endl;
             close(listenSocket);
             exit(EXIT_FAILURE);
         }
 
-        if (listen(listenSocket, SOMAXCONN) < 0) {
+        if (listen(listenSocket, SOMAXCONN) < 0)
+        {
             std::cerr << "Failed to listen on the socket." << std::endl;
             close(listenSocket);
             exit(EXIT_FAILURE);
@@ -103,21 +132,27 @@ int main(int argc, char** argv) {
         std::cout << "Server is listening on port " << serverConfig.port << std::endl;
     }
 
-    while (true) {
+    while (true)
+    {
         int ready = poll(&pollfds[0], pollfds.size(), -1);
-        if (ready < 0) {
+        if (ready < 0)
+        {
             std::cerr << "Failed to poll for events." << std::endl;
             exit(EXIT_FAILURE);
         }
 
-        for (std::vector<pollfd>::iterator it = pollfds.begin(); it != pollfds.end(); ++it) {
-            pollfd& pfd = *it;
-            if (pfd.revents & POLLIN) {
-                if (pfd.fd == listenSocket) {
+        for (std::vector<pollfd>::iterator it = pollfds.begin(); it != pollfds.end();)
+        {
+            pollfd &pfd = *it;
+            if (pfd.revents & POLLIN)
+            {
+                if (pfd.fd == listenSocket)
+                {
                     struct sockaddr_in clientAddress;
                     socklen_t clientAddressLength = sizeof(clientAddress);
-                    int clientSocket = accept(listenSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
-                    if (clientSocket < 0) {
+                    int clientSocket = accept(listenSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+                    if (clientSocket < 0)
+                    {
                         std::cerr << "Failed to accept the new connection." << std::endl;
                         exit(EXIT_FAILURE);
                     }
@@ -130,29 +165,89 @@ int main(int argc, char** argv) {
 
                     std::cout << "New client connected: " << inet_ntoa(clientAddress.sin_addr) << ":" << ntohs(clientAddress.sin_port) << std::endl;
                 }
-                else {
+                else
+                {
                     char buffer[1024];
                     int bytesRead = recv(pfd.fd, buffer, sizeof(buffer), 0);
-                    if (bytesRead <= 0) {
+                    if (bytesRead <= 0)
+                    {
                         if (bytesRead < 0)
                             std::cerr << "Failed to receive data on socket." << std::endl;
                         else
                             std::cout << "Client disconnected." << std::endl;
 
                         std::vector<int>::iterator clientSocketIter = std::find(clientSockets.begin(), clientSockets.end(), pfd.fd);
-                        if (clientSocketIter != clientSockets.end()) {
+                        if (clientSocketIter != clientSockets.end())
+                        {
                             clientSockets.erase(clientSocketIter);
                         }
 
                         close(pfd.fd);
                         it = pollfds.erase(it);
-                        --it;
+                        continue;
                     }
-                    else {
+                    else
+                    {
                         std::cout << "Received " << bytesRead << " bytes from client." << std::endl;
+
+                        // Process the request data
+                        std::string requestData(buffer, bytesRead);
+
+                        // Extract the HTTP method, URL, headers, and payload
+                        std::string httpMethod;
+                        std::string url;
+                        std::map<std::string, std::string> headers;
+                        std::string payload;
+
+                        // Parse the HTTP request
+                        // Split the request data by lines
+                        std::istringstream iss(requestData);
+                        std::string line;
+                        if (std::getline(iss, line))
+                        {
+                            // Split the first line by spaces to extract the HTTP method and URL
+                            std::istringstream lineIss(line);
+                            if (lineIss >> httpMethod >> url)
+                            {
+                                // Read the rest of the headers
+                                while (std::getline(iss, line) && !line.empty())
+                                {
+                                    size_t colonPos = line.find(':');
+                                    if (colonPos != std::string::npos)
+                                    {
+                                        std::string headerKey = line.substr(0, colonPos);
+                                        std::string headerValue = line.substr(colonPos + 2); // Skip the space after the colon
+                                        headers[headerKey] = headerValue;
+                                    }
+                                }
+
+                                // Read the payload if present
+                                if (!iss.eof())
+                                {
+                                    std::getline(iss, payload, '\0');
+                                }
+                            }
+                        }
+
+                        // Handle the request based on the HTTP method
+                        if (httpMethod == "GET")
+                        {
+                            handleGetRequest(pfd.fd, url);
+                        }
+                        else if (httpMethod == "POST")
+                        {
+                            handlePostRequest(pfd.fd, url, payload);
+                        }
+                        else
+                        {
+                            std::string response = "HTTP/1.1 501 Not Implemented\r\nContent-Type: text/plain\r\n\r\n";
+                            response += "Unsupported HTTP method: " + httpMethod;
+                            send(pfd.fd, response.c_str(), response.length(), 0);
+                        }
                     }
                 }
             }
+            ++it;
         }
     }
 
